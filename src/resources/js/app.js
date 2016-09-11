d00dd587291eac5be5b72d12a265f0648e7ef624
“Tinisi” <jgrosz@tinisi.com>
2016-09-11 17:16:15 -0500

    Successfully using fetch polyfill to hit API when running in docker
    * added the fetch polyfill to package.json and requiring it
    * changed route in express server to have /api prefix (matches nginx proxy pass rule)
    * added small proof of concept fetch call attached to button on form and markup for a text area
    * w00T! got a 200 response and correct text from server with no CORS issues!
    * TODO: having new problem with mapping local source folder to VM, so webpack build and docker-compose build needed, not so awesome
    * TODO: set up CORS rules so I can hit docker version of express server, or local version on different port from webpack dev server

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -6,19 +6,27 @@ class App {
     document.body.innerHTML = formHTML;
     // get the root element (this is in the templates we just loaded);
     this.root = document.querySelector('#app');
+    this.goButton = document.querySelector('#get-scoring-words');
+    this.boggleBoard = document.querySelector('#boggle-board');
     // get some css
     require('../styles/app.css');
-    // // set up a listener to do something interesting
-    // let self = this;
-    // let goHandler = function(e) {
-    //   self.printAwesomeMessage(self.textField.value);
-    // }
-    // this.goButton.addEventListener('click', goHandler);
+    // set up a listener to do something interesting
+    let self = this;
+    let goHandler = function(e) {
+      self.sendBoggleBoardToServer(self.boggleBoard.value);
+    }
+    this.goButton.addEventListener('click', goHandler);
   }
 
-  printAwesomeMessage(msg) {
-    // let awesomeMessage = awesome(msg);
-    // this.messageContainer.textContent = awesomeMessage;
+  sendBoggleBoardToServer(board) {
+    console.log('contents of textarea: ', board);
+    fetch('/api/users')
+      .then(function(response) {
+        console.log('raw response from server: ', response);
+        return response.text()
+      }).then(function(body) {
+        console.log('body: ', body);
+      });
   }
 
 }
